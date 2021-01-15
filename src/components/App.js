eea0434e6de0e03459990197361a7a47ba62e8f3
Giorgio <grggnwn@gmail.com>
2021-01-15 13:50:25 +1100

    basic API fetches can get image on console already

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-  onSearchSubmit(e){
-    console.log(e);
+  async onSearchSubmit(e){
+    const response = await axios.get('https://api.unsplash.com/search/photos',{
+      params:{
+        query:e
+      },
+      headers: {
+        Authorization: 'Client-ID f8UsSrI-4xYEtCzR4GEILlOptKu5iYxR3J5aRU4uQWU'
+      }
+    });
+
+    console.log(response.data.results);
   }
   render(){
     return(
