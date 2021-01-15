bdd56e28cccc046f4fdbeaccb335aafd9c4c510f
Giorgio <grggnwn@gmail.com>
2021-01-16 01:17:36 +1100

    images can be shown

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList'
 
 class App extends React.Component {
   state = { images: [] };
@@ -9,14 +10,14 @@ class App extends React.Component {
     const response = await unsplash.get('search/photos',{
       params:{query:e}
     });
-    
+
     this.setState({images: response.data.results}); // will cause component to re render
   }
   render(){
     return(
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={ this.onSearchSubmit}/>
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images}/>
       </div>
     );
   }
