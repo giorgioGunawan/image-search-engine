3f74c58d34c6d1a0630a2289bc07bda9c6c24ce1
Giorgio <grggnwn@gmail.com>
2021-01-16 13:56:29 +1100

    App done

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const ImageList = (props) =>{
 
-    const images = props.images.map((image) => {
-        return <img src={image.urls.regular} />
+    const images = props.images.map(({description,id,urls}) => {
+        return <img altprop={description} key={id} src={urls.regular} />
     });
 
 
